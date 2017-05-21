52976c10d396381c317c0fb490719a88c7389af1
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-05-21 12:02:45 +0000

    Сonstrain master branch to 0.3 version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 let package = Package(
     name: "Async",
     dependencies: [
-        .Package(url: "https://github.com/swift-stack/platform.git", majorVersion: 0)
+        .Package(
+            url: "https://github.com/swift-stack/platform.git",
+            majorVersion: 0,
+            minor: 3
+        )
     ]
 )
