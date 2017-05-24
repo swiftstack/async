f11c20e307bf8054fc6245a82823f0dad48b7695
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-05-24 10:51:57 +0000

    Add syncTask

--- Sources/Async/Async.swift
+++ Sources/Async/Async.swift
@@ -1,11 +1,19 @@
 import struct Foundation.Date
+import struct Dispatch.DispatchQoS
 
 public typealias AsyncTask = () -> Void
 
 public protocol Async {
     var loop: AsyncLoop { get }
-    var task: (@escaping AsyncTask) -> Void { get }
     var awaiter: IOAwaiter? { get }
+
+    func task(_ closure: @escaping AsyncTask) -> Void
+
+    func syncTask<T>(
+        qos: DispatchQoS.QoSClass,
+        deadline: Date,
+        task: @escaping () throws -> T
+    ) throws -> T
 }
 
 public protocol AsyncLoop {
