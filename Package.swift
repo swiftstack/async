12dd426aca8e94be0f0c8f7e237eb2aa512a95fe
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-24 06:00:16 +0000

    [Package] Add multiple package sources

--- Package.swift
+++ Package.swift
@@ -4,21 +4,67 @@ import PackageDescription
 let package = Package(
     name: "Async",
     products: [
-        .library(name: "Async", targets: ["Async"])
+        .library(
+            name: "Async",
+            targets: ["Async"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/swift-stack/platform.git",
-            .branch("master")),
-        .package(
-            url: "https://github.com/swift-stack/time.git",
-            .branch("master")),
-        .package(
-            url: "https://github.com/swift-stack/test.git",
-            .branch("master"))
+        .package(name: "Platform"),
+        .package(name: "Time"),
+        .package(name: "Test")
     ],
     targets: [
-        .target(name: "Async", dependencies: ["Platform", "Time"]),
-        .testTarget(name: "AsyncTests", dependencies: ["Async", "Test"])
+        .target(
+            name: "Async",
+            dependencies: ["Platform", "Time"]),
+        .testTarget(
+            name: "AsyncTests",
+            dependencies: ["Test", "Async"])
     ]
 )
+
+// MARK: - custom package source
+
+#if canImport(ObjectiveC)
+import Darwin.C
+#else
+import Glibc
+#endif
+
+extension Package.Dependency {
+    enum Source: String {
+        case local, remote, github
+
+        static var `default`: Self { .local }
+
+        var baseUrl: String {
+            switch self {
+            case .local: return "../"
+            case .remote: return "https://swiftstack.io/"
+            case .github: return "https://github.com/swift-stack/"
+            }
+        }
+
+        func url(for name: String) -> String {
+            return self == .local
+                ? baseUrl + name.lowercased()
+                : baseUrl + name.lowercased() + ".git"
+        }
+    }
+
+    static func package(name: String) -> Package.Dependency {
+        guard let pointer = getenv("SWIFTSTACK") else {
+            return .package(name: name, source: .default)
+        }
+        guard let source = Source(rawValue: String(cString: pointer)) else {
+            fatalError("Invalid source. Use local, remote or github")
+        }
+        return .package(name: name, source: source)
+    }
+
+    static func package(name: String, source: Source) -> Package.Dependency {
+        return source == .local
+            ? .package(name: name, path: source.url(for: name))
+            : .package(name: name, url: source.url(for: name), .branch("dev"))
+    }
+}
